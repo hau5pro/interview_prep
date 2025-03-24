6caa6f8ad015ebfdc2611155019e4734b62f9a09
House Yokeswaran <h4u5d3v@gmail.com>
2025-03-24 14:49:35 -0400

    7 valid anagram

---
+++ questions/7_valid_anagram.ts
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/valid-anagram/
+// #string #hash_map
+
+async function script7() {
+  // script here
+  // const res = isAnagram("anagram", "nagaram");
+  const res = isAnagram("rat", "car");
+  console.log(res);
+
+  console.log("done");
+  process.exit(0);
+}
+
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const map: { [key: string]: number } = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (map[char]) {
+      map[char] = map[char] + 1;
+    } else {
+      map[char] = 1;
+    }
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    const char = t[i];
+    if (map[char]) {
+      const diff = map[char] - 1;
+      if (diff === 0) {
+        delete map[char];
+      } else {
+        map[char] = map[char] - 1;
+      }
+    } else {
+      break;
+    }
+  }
+
+  const isAnagram = Object.keys(map).length === 0;
+
+  return isAnagram;
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+script7().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
