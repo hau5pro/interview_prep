daa5910a86f70b559d54c84d12c5ecf230647d20
House Yokeswaran <h4u5d3v@gmail.com>
2025-04-25 11:26:40 -0400

    24 contains duplicate

---
+++ questions/24_contains_duplicate.ts
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/contains-duplicate/
+// #array
+
+async function script24() {
+  // script here
+  const res = containsDuplicate([2, 14, 18, 22, 22]);
+  console.log(res);
+
+  console.log("done");
+  process.exit(0);
+}
+
+// time O(n)
+// space O(n)
+function containsDuplicate(nums: number[]): boolean {
+  const lookup = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (lookup.has(nums[i])) return true;
+    lookup.add(nums[i]);
+  }
+
+  return false;
+}
+
+// time O(log(n)) its slow for some reason?
+// space O(1)
+
+// function containsDuplicate(nums: number[]): boolean {
+//   nums.sort((a, b) => a - b);
+
+//   let prev: number;
+//   let containsDuplicate = false;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (i === 0) {
+//       prev = nums[i];
+//     } else {
+//       if (nums[i] === prev) {
+//         containsDuplicate = true;
+//         break;
+//       } else {
+//         prev = nums[i];
+//       }
+//     }
+//   }
+
+//   return containsDuplicate;
+// }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+script24().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
