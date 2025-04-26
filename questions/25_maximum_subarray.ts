5a6409d77f7aee2102597eb62e3a757946e8f9ac
House Yokeswaran <h4u5d3v@gmail.com>
2025-04-26 11:17:05 -0400

    25 maximum subarray

---
+++ questions/25_maximum_subarray.ts
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/maximum-subarray/
+// #array
+
+async function script25() {
+  // script here
+  const res = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+  console.log(res);
+
+  console.log("done");
+  process.exit(0);
+}
+
+// time O(n)
+// space O(1)
+
+function maxSubArray(nums: number[]): number {
+  let local = 0;
+  let global = -Infinity;
+
+  for (const num of nums) {
+    local = Math.max(num, local + num);
+    if (local > global) {
+      global = local;
+    }
+  }
+
+  return global;
+}
+
+// time O(n^2)
+// space O(1)
+
+// function maxSubArray(nums: number[]): number {
+//   let max = nums[0];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let sum = nums[i];
+
+//     for (let j = i + 1; j < nums.length; j++) {
+//       sum += nums[j];
+//       max = Math.max(sum, max, nums[j]);
+//     }
+//   }
+
+//   return max;
+// }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+script25().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
